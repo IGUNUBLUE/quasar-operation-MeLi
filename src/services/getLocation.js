f81deebecb68c8af884434da23566100b24cc5dd
Lenin Garizabalo <lcamargo@protonmail.com>
2021-06-06 22:29:35 -0500

    fix getLocation

--- src/services/getLocation.js
+++ src/services/getLocation.js
@@ -5,36 +5,51 @@ const satCurrentPosition = {
 };
 
 function getLocation(satellites) {
-  var xa = satCurrentPosition[satellites[0].name].position.x;
-  var ya = satCurrentPosition[satellites[0].name].position.y;
-  var xb = satCurrentPosition[satellites[1].name].position.x;
-  var yb = satCurrentPosition[satellites[1].name].position.y;
-  var xc = satCurrentPosition[satellites[2].name].position.x;
-  var yc = satCurrentPosition[satellites[2].name].position.y;
-  var ra = satellites[0].distance;
-  var rb = satellites[1].distance;
-  var rc = satellites[2].distance;
+  let x1 = satCurrentPosition[satellites[0].name].position.x;
+  let y1 = satCurrentPosition[satellites[0].name].position.y;
+  let x2 = satCurrentPosition[satellites[1].name].position.x;
+  let y2 = satCurrentPosition[satellites[1].name].position.y;
+  let x3 = satCurrentPosition[satellites[2].name].position.x;
+  let y3 = satCurrentPosition[satellites[2].name].position.y;
+  let d1 = satellites[0].distance;
+  let d2 = satellites[1].distance;
+  let d3 = satellites[2].distance;
 
-  var S =
-    (Math.pow(xc, 2) -
-      Math.pow(xb, 2) +
-      Math.pow(yc, 2) -
-      Math.pow(yb, 2) +
-      Math.pow(rb, 2) -
-      Math.pow(rc, 2)) /
-    2.0;
-  var T =
-    (Math.pow(xa, 2) -
-      Math.pow(xb, 2) +
-      Math.pow(ya, 2) -
-      Math.pow(yb, 2) +
-      Math.pow(rb, 2) -
-      Math.pow(ra, 2)) /
-    2.0;
-  var y =
-    (T * (xb - xc) - S * (xb - xa)) /
-    ((ya - yb) * (xb - xc) - (yc - yb) * (xb - xa));
-  var x = (y * (ya - yb) - T) / (xb - xa);
+  //==> X
+  let tempCalOne =
+    (Math.pow(y2, 2) -
+      Math.pow(y1, 2) +
+      (Math.pow(x2, 2) - Math.pow(x1, 2)) +
+      (Math.pow(d1, 2) - Math.pow(d2, 2))) *
+    (y2 - y3);
+
+  var tempCalTwo =
+    Math.pow(y3, 2) -
+    Math.pow(y2, 2) +
+    (Math.pow(x3, 2) - Math.pow(x2, 2)) +
+    (Math.pow(d2, 2) - Math.pow(d3, 2)) * (y1 - y2);
+
+  let tempCalThree = (x1 - x2) * (y2 - y3) - (x2 - x3) * (y1 - y2);
+
+  let x = (tempCalOne - tempCalTwo) / (tempCalThree * 2);
+
+ //==> Y
+  tempCalOne =
+    (Math.pow(x2, 2) -
+      Math.pow(x1, 2) +
+      (Math.pow(y2, 2) - Math.pow(y1, 2)) +
+      (Math.pow(d1, 2) - Math.pow(d2, 2))) *
+    (x2 - x3);
+
+  tempCalTwo =
+    Math.pow(x3, 2) -
+    Math.pow(x2, 2) +
+    (Math.pow(y3, 2) - Math.pow(y2, 2)) +
+    (Math.pow(d2, 2) - Math.pow(d3, 2)) * (x1 - x2);
+
+  tempCalThree = (y1 - y2) * (x2 - x3) - (y2 - y3) * (x1 - x2);
+
+  let y = (tempCalOne - tempCalTwo) / (tempCalThree * 2);
 
   return {
     x: x,
